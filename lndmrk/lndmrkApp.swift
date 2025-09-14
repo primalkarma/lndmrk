dec7580148ce77efc0f2df8b4f7bb8a11888282a
primalkarma <primalkarma@gmail.com>
2025-09-14 20:48:35 +0530

    Initial Commit

---
+++ lndmrk/lndmrkApp.swift
@@ -0,0 +1,17 @@
+//
+//  lndmrkApp.swift
+//  lndmrk
+//
+//  Created by Parimal Devi on 14/09/25.
+//
+
+import SwiftUI
+
+@main
+struct lndmrkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
